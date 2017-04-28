037d4433b22ed1037f710a6745b36467acd94e64
Bathige, Susantha <bathige@hotmail.com>
2017-04-28 14:05:18 -0600

    test mongo playbook changes

--- temp.sh
+++ temp.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -x
 cd /root
-git clone https://github.com/Susanthab/my_projects.git
+git clone https://github.com/Susanthab/my_projects.git mongo
 cd /root
+pip install -q -r ./mongo/ansible-roles/requirements.txt
+ansible-playbook -i localhost /root/mongo/ansible-roles/test_install_mongo.yaml
