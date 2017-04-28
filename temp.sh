370a3c1d764aa9d2630dfd79a1127c3f1dab638f
Bathige, Susantha <bathige@hotmail.com>
2017-04-28 14:39:06 -0600

    test PB changes for Cloud ini exec

--- temp.sh
+++ temp.sh
@@ -4,4 +4,4 @@ cd /root
 git clone https://github.com/Susanthab/my_projects.git mongo
 cd /root
 pip install -q -r ./mongo/ansible-roles/requirements.txt
-ansible-playbook -i localhost /root/mongo/ansible-roles/test_install_mongo.yaml
+ansible-playbook -i "localhost," -c local /root/mongo/ansible-roles/test_install_mongo.yaml
