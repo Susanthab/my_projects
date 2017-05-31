f1561612a9a18fbadbcb25abdb8b167cf78662fb
Bathige, Susantha <bathige@hotmail.com>
2017-05-31 16:03:11 -0600

    changes to u.d file

--- aws/userdata_lc.sh
+++ aws/userdata_lc.sh
@@ -11,9 +11,6 @@ mkfs -t ext4 $P
 mkdir /data
 mount $P /data
 
-cd /root
-git clone https://github.com/Susanthab/my_projects.git mongo
-cd /root
 pip install -q -r ./mongo/ansible-roles/requirements.txt
 ansible-playbook -i "localhost," -c local /root/mongo/ansible-roles/test_install_mongo.yaml
 
--- temp.sh
+++ temp.sh
@@ -1,77 +1,8 @@
-parameters:
-  Region:
-    type: string
-    required: true
-    position: 0
-    default: "us-east-1"
-  AvailabilityZones:
-    type: array
-    required: true
-    position: 1
-    default: ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e"]
-  LaunchConfigurationName:
-    type: string
-    required: true
-    position: 2
-  Asg_name:
-    required: true
-    type: string
-    position: 3
-  Environment:
-    required: true
-    type: string
-    position: 4
-  App_type:
-    required: true
-    type: string
-    position: 5
-  Team: 
-    type: string
-    required: true
-    position: 6
-  MinSize:
-    type: integer
-    required: true
-    position: 7
-    default: 3
-  DesiredCapacity:
-    type: integer
-    required: true
-    position: 8
-    default: 2
-  MaxSize:
-    type: integer
-    required: true
-    position: 9
-    default: 7
-  ImageId:
-    required: false
-    type: string
-    position: 10
-    default: "ami-47bc4c51"
-  KeyName:
-    required: false
-    type: string
-    position: 11
-    default: "susanthab"
-  SecurityGroups:
-    required: false
-    type: array
-    position: 12
-    default: "sg-06249479"
-  IamInstanceProfile:
-    required: false
-    type: string
-    position: 13
-    default: "MongoDBRole"
-  EbsOptimized:
-    required: false
-    type: string
-    position: 14
-    default: false
-  InstanceType:
-    required: false
-    type: string
-    position: 15
-    default: "t2.micro"
-runner_type: mistral-v2
+
+
+#! /bin/bash
+#cloud-config
+output: {all: '| tee -a /var/log/cloud-init-output.log'}
+set -x
+cd /root
+git clone https://github.com/Susanthab/my_projects.git mongo
