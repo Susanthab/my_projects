90f51c478548c62e55272509161130047f838d1e
Bathige, Susantha <bathige@hotmail.com>
2017-07-07 16:34:25 -0600

    user data

--- aws/userdata_lc_privateIp.sh
+++ aws/userdata_lc_privateIp.sh
@@ -3,7 +3,8 @@
 output: {all: '| tee -a /var/log/cloud-init-output.log'}
 set -x
 
-sleep 5
+# Random sleep between 1 - 10 sec. 
+sleep $[ ( $RANDOM % 10 )  + 1 ]s
 
 echo "Pragramatically mount block device at EC2 startup..."
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-using-volumes.html
@@ -17,7 +18,8 @@ UUID=`blkid | grep $umd | awk -F'UUID="' '{print $2}' | awk -F'"' '{print $1}'`
 echo "UUID=$UUID       /data   ext4    defaults,nofail        0       2" >> /etc/fstab
 mount -a
 
-sleep 5
+# Random sleep between 1 - 10 sec. 
+sleep $[ ( $RANDOM % 10 )  + 1 ]s
 
 pip install -q -r ./mongo/ansible-roles/requirements.txt
 ansible-playbook -i "localhost," -c local /root/mongo/ansible-roles/test_install_mongo.yaml
@@ -27,7 +29,7 @@ sleep $[ ( $RANDOM % 10 )  + 1 ]s
 
 # Use below script is to initialize the replica set in ASG. 
 wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
-sudo chmod u+x ec2-metadata
+chmod u+x ec2-metadata
 EC2_AVAIL_ZONE=$(./ec2-metadata -z | grep -Po "(us|sa|eu|ap)-(north|south|central)?(east|west)?-[0-9]+")
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 INSTANCE_ID=$(./ec2-metadata | grep instance-id | awk 'NR==1{print $2}')
@@ -37,8 +39,10 @@ SECONDARY="false"
 for ID in $(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${AG_NAME} --region ${EC2_REGION} --query AutoScalingGroups[].Instances[].InstanceId --output text);
 do
    IP=$(aws ec2 describe-instances --instance-ids $ID --region ${EC2_REGION} --query Reservations[].Instances[].PrivateIpAddress --output text)
+   echo $IP
    echo "Checking for the Primary node IP."
    P=`/usr/bin/mongo ${IP}:27017 --eval "printjson(rs.isMaster())" | grep "primary" | cut -d"\"" -f4`
+   echo $P
    if [ -n "$P" ]; then
       echo "Primary node found, record the PRIMARY node IP"
       PRIMARY=${P}
@@ -71,11 +75,12 @@ if [ -n "$PRIMARY" ]; then
     echo "Join the new secondary to the existing replica set."
     R=`/usr/bin/mongo ${PRIMARY}/admin --eval "printjson(rs.add('${CURRENT_NODE_IP}:27017'))"`
     echo $R
+    
 elif [ "$SECONDARY" = "false" ]; then
     # Initialize replica set
     echo "No primary, initialize the replica set."
     cfg="{_id: 'rs0', members: [{_id: 0, host: '${CURRENT_NODE_IP}:27017'}]}"
-    # R=`/usr/bin/mongo ${CURRENT_NODE_IP}/admin --eval "printjson(rs.initiate($cfg))"`
+    R=`/usr/bin/mongo ${CURRENT_NODE_IP}/admin --eval "printjson(rs.initiate($cfg))"`
     echo $cfg
     echo $R
 else
