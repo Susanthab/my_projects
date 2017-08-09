63748f6f787a24212b0885c7a2442c9f6761c490
Bathige, Susantha <bathige@hotmail.com>
2017-08-09 15:57:58 -0600

    user data and ansible changes.

--- aws/user-data/cassandra_ring.sh
+++ aws/user-data/cassandra_ring.sh
@@ -24,3 +24,16 @@ mount -a
 pip install -q -r ./cassandra/ansible-roles/requirements.txt
 ansible-playbook -i "localhost," -c local /root/cassandra/ansible-roles/roles/apache-cassandra-nibiru/test.yml
 #ansible-playbook -i "localhost," -c local test.yml
+
+# Use below script is to ASG meta data. 
+wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
+chmod u+x ec2-metadata
+EC2_AVAIL_ZONE=$(./ec2-metadata -z | grep -Po "(us|sa|eu|ap)-(north|south|central)?(east|west)?-[0-9]+")
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+INSTANCE_ID=$(./ec2-metadata | grep instance-id | awk 'NR==1{print $2}')
+CURRENT_NODE_IP=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --region ${EC2_REGION} --query Reservations[].Instances[].PrivateIpAddress --output text)
+AG_NAME=$(aws autoscaling describe-auto-scaling-instances --instance-ids ${INSTANCE_ID} --region ${EC2_REGION} --query AutoScalingInstances[].AutoScalingGroupName --output text)
+
+#update Cassandra.yaml
+#1. Listen_address to private IP of the local host. 
+sed -i "s/listen_address: localhost/listen_address: $CURRENT_NODE_IP/g" /etc/cassandra/cassandra.yaml
