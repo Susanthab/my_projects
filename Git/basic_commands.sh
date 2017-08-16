1edaae3d28e63b06176698e6a2e95aba5aea65f8
Bathige, Susantha <bathige@hotmail.com>
2017-08-16 16:16:29 -0600

    cass ansible

--- Git/basic_commands.sh
+++ Git/basic_commands.sh
@@ -2,3 +2,11 @@
 
 # create local branch.
 git checkout -b <branch-name>
+
+# add files
+
+# commit files. 
+git commit -m "st2 workflow to create cassandra ring."
+
+# how to push a branch to remote remote.
+git push -u origin nib-228
