ef13da91c99410e5777c71918f551cc5304e9e7c
Bathige, Susantha <bathige@hotmail.com>
2017-10-27 10:32:10 -0600

    couch.b us

--- aws/user-data/couchbase_userdata.sh
+++ aws/user-data/couchbase_userdata.sh
@@ -123,10 +123,9 @@ cluster_init () {
     # Need to find a method to protect this password. Future work. 
     echo "Server type of the node: $SERVICE_TYPE"
     
-    if [ "$SERVICE_TYPE" == "AllServicesInOne" ]
-    then
+    if [ "$SERVICE_TYPE" == "AllServicesInOne" ]; then
         ip=$ALL_ASG_INST[0]
-        if [ "$ip" == "$CURRENT_NODE_IP" ]
+        if [ "$ip" == "$CURRENT_NODE_IP" ]; then
             output=null
             output=$(/opt/couchbase/bin/couchbase-cli cluster-init -c $CURRENT_NODE_IP --cluster-username $CLUSTER_USER_NAME \
                     --cluster-password $CLUSTER_PASSWORD --cluster-name $CLUSTER_NAME --services data,index,query \
