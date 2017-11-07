820b3d80b71e91cf07599a1bc4d4ce1f10323180
Bathige, Susantha <bathige@hotmail.com>
2017-11-07 15:54:58 -0700

    couchb ud

--- aws/user-data/couchbase_userdata.sh
+++ aws/user-data/couchbase_userdata.sh
@@ -14,7 +14,7 @@
 mkdir /tmp/ssm
 wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
 dpkg -i amazon-ssm-agent.deb
-echo "check to see whether SSM agent is running..."
+echo "INFO: check to see whether SSM agent is running..."
 status=$(service amazon-ssm-agent status | grep "running" | awk '{print$3}')
 if [ "$status" == "(running)" ]; then
     echo "$service is running!!!"
@@ -24,7 +24,7 @@ else
 fi
 ## ***************************************************************************************************
 
-echo "Pragramatically mount block device at EC2 startup..."
+echo "INFO: Pragramatically mount block device at EC2 startup..."
 # http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ebs-using-volumes.html
 # The following code is only to add one device. 
 cp /etc/fstab /etc/fstab.orig
@@ -36,14 +36,14 @@ UUID=`blkid | grep $umd | awk -F'UUID="' '{print $2}' | awk -F'"' '{print $1}'`
 echo "UUID=$UUID       /data   ext4    defaults,nofail        0       2" >> /etc/fstab
 mount -a
 
-echo "Install pre-requisites..."
+echo "INFO: Install pre-requisites..."
 # NOTE: This has to be changed to a proper location. 
 pip install -q -r ./couchbase/ansible-roles/requirements.txt
 # upgrade awscli to get new features 
 pip install awscli --upgrade
 
 # Use below script is to get ASG meta data. 
-echo "Get AWS metadata..."
+echo "INFO: Get AWS metadata..."
 wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
 chmod u+x ec2-metadata
 AZ=$(./ec2-metadata -z)
@@ -53,17 +53,17 @@ INSTANCE_ID=$(./ec2-metadata | grep instance-id | awk 'NR==1{print $2}')
 CURRENT_NODE_IP=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --region ${EC2_REGION} --query Reservations[].Instances[].PrivateIpAddress --output text)
 AG_NAME=$(aws autoscaling describe-auto-scaling-instances --instance-ids ${INSTANCE_ID} --region ${EC2_REGION} --query AutoScalingInstances[].AutoScalingGroupName --output text)
 
-echo "Change the swappiness value to 0 and make it Permanent.."
+echo "INFO: Change the swappiness value to 0 and make it Permanent.."
 echo 0 > /proc/sys/vm/swappiness
 echo '' >> /etc/sysctl.conf 
 echo '#Set swappiness to 0 to avoid swapping' >> /etc/sysctl.conf 
 echo 'vm.swappiness = 0' >> /etc/sysctl.conf 
 
-echo "Disable Transparent Huge Pages..."
+echo "INFO: Disable Transparent Huge Pages..."
 echo never > /sys/kernel/mm/transparent_hugepage/enabled 
 echo never > /sys/kernel/mm/transparent_hugepage/defrag 
 
-echo "Install Couchbase..."
+echo "INFO: Install Couchbase..."
 wget https://packages.couchbase.com/releases/4.6.3/couchbase-server-enterprise_4.6.3-ubuntu14.04_amd64.deb
 dpkg -i couchbase-server-enterprise_4.6.3-ubuntu14.04_amd64.deb
 
@@ -89,13 +89,13 @@ create_paths () {
 get_tags_and_instances () {
     SERVICE_TYPE=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${AG_NAME} \
         --region ${EC2_REGION} --query 'AutoScalingGroups[].Tags[?Key==`service_type`].{val:Value}' --output text | head -n1 | awk '{print $1;}');
-    echo "Node service type: $SERVICE_TYPE"
+    echo "INFO: Node service type: $SERVICE_TYPE"
     SERVICE_OFFERING=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${AG_NAME} \
         --region ${EC2_REGION} --query 'AutoScalingGroups[].Tags[?Key==`service_offering`].{val:Value}' --output text | head -n1 | awk '{print $1;}');
-    echo "Node service offering: $SERVICE_OFFERING"
+    echo "INFO: Node service offering: $SERVICE_OFFERING"
     CLUSTER_NAME=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${AG_NAME} \
         --region ${EC2_REGION} --query 'AutoScalingGroups[].Tags[?Key==`cluster_name`].{val:Value}' --output text | head -n1 | awk '{print $1;}');
-    echo "Cluster name: $CLUSTER_NAME"
+    echo "INFO: Cluster name: $CLUSTER_NAME"
 
     if [ "$SERVICE_TYPE" == "AllServicesInOne" ]; then
         ALL_ASG_INST=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${AG_NAME} \
@@ -107,17 +107,17 @@ get_tags_and_instances () {
         ASG_DATA_NAME="couchbase-asg-data-$CLUSTER_NAME"
         ASG_DATA_INST=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${ASG_DATA_NAME} \
         --region ${EC2_REGION} --query AutoScalingGroups[].Instances[].InstanceId --output text);
-        echo "Data service instances of the cluster: $ASG_DATA_INST"
+        echo "INFO: Data service instances of the cluster: $ASG_DATA_INST"
 
         ASG_INDEX_NAME="couchbase-asg-index-$CLUSTER_NAME"
         ASG_INDEX_INST=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${ASG_INDEX_NAME} \
         --region ${EC2_REGION} --query AutoScalingGroups[].Instances[].InstanceId --output text);
-        echo "Index service instances of the cluster: $ASG_INDEX_INST"
+        echo "INFO: Index service instances of the cluster: $ASG_INDEX_INST"
 
         ASG_QUERY_NAME="couchbase-asg-query-$CLUSTER_NAME"
         ASG_QUERY_INST=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${ASG_QUERY_NAME} \
         --region ${EC2_REGION} --query AutoScalingGroups[].Instances[].InstanceId --output text);
-        echo "Query service instances of the cluster: $ASG_QUERY_INST"
+        echo "INFO: Query service instances of the cluster: $ASG_QUERY_INST"
 
         echo "Concatenate all the instances of the auto-scaling groups into one."
         ALL_ASG_INST="$ASG_DATA_INST $ASG_INDEX_INST $ASG_QUERY_INST"
@@ -141,9 +141,9 @@ wait_for_network () {
     do
         ip=$(aws ec2 describe-instances --instance-ids $id --region ${EC2_REGION} --query \
         Reservations[].Instances[].PrivateIpAddress --output text)
-        echo "Waiting network on $ip"
+        echo "INFO: Waiting network on $ip"
         while ! ping -c 1 -W 1 $ip; do
-            echo "Waiting for $ip - network interface might be down..."
+            echo "INFO: Waiting for $ip - network interface might be down..."
             sleep 1
         done
     done
@@ -156,7 +156,7 @@ node_init () {
     output=null
     output=$(/opt/couchbase/bin/couchbase-cli node-init -c $CURRENT_NODE_IP --node-init-data-path "$DATA_PATH" \
     --node-init-index-path "$INDEX_PATH" -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD)
-    echo "output: node-init: $output"
+    echo "OUTPUT: node-init: $output"
 }
 ## ***************************************************************************************************
 
@@ -177,14 +177,14 @@ cluster_init () {
     
     ip=$(get_node_ip $arg1 $id)       
     #PRIMARY_SERVER_IP=$ip  
-    echo "Designated primary server ip: $PRIMARY_SERVER_IP"  
+    echo "INFO: Designated primary server ip: $PRIMARY_SERVER_IP"  
     if [ "$ip" == "$CURRENT_NODE_IP" -a "$SERVICE_OFFERING" == "data" -o "$SERVICE_TYPE" == "AllServicesInOne" ]; then
 
         if [ "$SERVICE_OFFERING" == "data" ]; then
             output=$(/opt/couchbase/bin/couchbase-cli cluster-init -c $CURRENT_NODE_IP --cluster-username $CLUSTER_USER_NAME \
                     --cluster-password $CLUSTER_PASSWORD --cluster-name $CLUSTER_NAME --services data \
                     --cluster-ramsize 256 )
-            echo "output: cluster-init: $output"
+            echo "OUTPUT: cluster-init: $output"
             # Ideally the cluster name should be set with cluster-init method. However due to some reason it seems not working. 
             #So setting cluster name in a different way.
         fi
@@ -193,22 +193,22 @@ cluster_init () {
             output=$(/opt/couchbase/bin/couchbase-cli cluster-init -c $CURRENT_NODE_IP --cluster-username $CLUSTER_USER_NAME \
                     --cluster-password $CLUSTER_PASSWORD --cluster-name $CLUSTER_NAME --services data,index,query \
                     --cluster-ramsize 256 --cluster-index-ramsize 256)
-            echo "output: cluster-init: $output"
+            echo "OUTPUT: cluster-init: $output"
         fi
 
         output=$(/opt/couchbase/bin/couchbase-cli setting-cluster -c $CURRENT_NODE_IP -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD \
                 --cluster-name $CLUSTER_NAME)
-        echo "output: setting-cluster (cluster name): $output"
+        echo "OUTPUT: setting-cluster (cluster name): $output"
 
         # Move node to a proper group. 
         # Create a group and then move the node because rename group did not work. 
         group_name=`echo "rack-"${AZ:(-2)}`
         output=$(/opt/couchbase/bin/couchbase-cli group-manage -c $CURRENT_NODE_IP -u $CLUSTER_USER_NAME \
             -p $CLUSTER_PASSWORD --create --group-name $group_name)
-        echo "output: create-group: $output"
+        echo "OUTPUT: create-group: $output"
         output=$(/opt/couchbase/bin/couchbase-cli group-manage -c $CURRENT_NODE_IP -u $CLUSTER_USER_NAME \
             -p $CLUSTER_PASSWORD --move-servers $CURRENT_NODE_IP --from-group "Group 1" --to-group $group_name)
-        echo "output: move-group: $output"
+        echo "OUTPUT: move-group: $output"
     fi
 }
 ## ***************************************************************************************************
@@ -237,7 +237,7 @@ wait_for_couchbase () {
             server_status=$(/opt/couchbase/bin/couchbase-cli server-info -c $ip -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD | \
                 grep status | awk '{print $2}' | tr -d '",')
         done
-        echo "Status of the server, $ip: $server_status"
+        echo "INFO: Status of the server, $ip: $server_status"
     done
     
 }
@@ -246,43 +246,43 @@ wait_for_couchbase () {
 ## ***************************************************************************************************
 server_add () {
 
-    echo "Service offering of the node: $SERVICE_OFFERING"
+    echo "INFO: Service offering of the node: $SERVICE_OFFERING"
     group_name=`echo "rack-"${AZ:(-2)}`
     output=$(/opt/couchbase/bin/couchbase-cli group-manage -c $PRIMARY_SERVER_IP -u $CLUSTER_USER_NAME \
                     -p $CLUSTER_PASSWORD --create --group-name $group_name)
-    echo "output: create-group: $output"
+    echo "OUTPUT: create-group: $output"
 
     if [ "$CURRENT_NODE_IP" != "$PRIMARY_SERVER_IP" -a "$SERVICE_OFFERING" == "data" ]; then
         output=$(/opt/couchbase/bin/couchbase-cli server-add --server-add=$CURRENT_NODE_IP --server-add-username=$CLUSTER_USER_NAME \
             --server-add-password=$CLUSTER_PASSWORD --group-name="$group_name" --services="data" \
             --cluster=$PRIMARY_SERVER_IP --user=$CLUSTER_USER_NAME --password=$CLUSTER_PASSWORD)
-        echo "output: server-add: $output"
+        echo "OUTPUT: server-add: $output"
     fi
 
     if [ "$SERVICE_OFFERING" == "index" ]; then
         output=$(/opt/couchbase/bin/couchbase-cli server-add --server-add=$CURRENT_NODE_IP --server-add-username=$CLUSTER_USER_NAME \
             --server-add-password=$CLUSTER_PASSWORD --group-name="$group_name" --services="index" \
             --cluster=$PRIMARY_SERVER_IP --user=$CLUSTER_USER_NAME --password=$CLUSTER_PASSWORD)
-        echo "output: server-add: $output"
+        echo "OUTPUT: server-add: $output"
     fi
 
     if [ "$SERVICE_OFFERING" == "query" ]; then
         output=$(/opt/couchbase/bin/couchbase-cli server-add --server-add=$CURRENT_NODE_IP --server-add-username=$CLUSTER_USER_NAME \
             --server-add-password=$CLUSTER_PASSWORD --group-name="$group_name" --services="query" \
             --cluster=$PRIMARY_SERVER_IP --user=$CLUSTER_USER_NAME --password=$CLUSTER_PASSWORD)
-        echo "output: server-add: $output"
+        echo "OUTPUT: server-add: $output"
     fi
 
     if [ "$CURRENT_NODE_IP" != "$PRIMARY_SERVER_IP" -a "$SERVICE_TYPE" == "AllServicesInOne" ]; then
         output=$(/opt/couchbase/bin/couchbase-cli server-add --server-add=$CURRENT_NODE_IP --server-add-username=$CLUSTER_USER_NAME \
             --server-add-password=$CLUSTER_PASSWORD --group-name="$group_name" --services="data","query","index" \
             --cluster=$PRIMARY_SERVER_IP --user=$CLUSTER_USER_NAME --password=$CLUSTER_PASSWORD)
-        echo "output: server-add: $output"   
+        echo "OUTPUT: server-add: $output"   
     fi
 
     echo "Checking whether server is added to the cluster..."
     output=$(/opt/couchbase/bin/couchbase-cli host-list --cluster $PRIMARY_SERVER_IP -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD | grep $CURRENT_NODE_IP)
-    echo "output: host-list: $output"
+    echo "OUTPUT: host-list: $output"
     while [ -z "$output" ]
     do
         sleep 5s
@@ -297,14 +297,14 @@ rebalance () {
     # wait for all the nodes of the Couchbase cluster. 
     wait_for_couchbase
     output=$(/opt/couchbase/bin/couchbase-cli rebalance -c  $PRIMARY_SERVER_IP -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD)
-    echo "output: rebalance: $output"
+    echo "OUTPUT: cluster rebalance: $output"
 }
 
 ## ***************************************************************************************************
 get_primary_server () {
 
-    echo "Service type of the node is: $SERVICE_TYPE" 
-    echo "Service offering of the node: $SERVICE_OFFERING"
+    echo "INFO: Service type of the node is: $SERVICE_TYPE" 
+    echo "INFO: Service offering of the node: $SERVICE_OFFERING"
 
     if [ "$SERVICE_TYPE" == "MultiDimentional" ]; then
         ALL_INST=$ASG_DATA_INST
@@ -319,13 +319,13 @@ get_primary_server () {
         ip=$(get_node_ip $arg1 $id)
         output=$(/opt/couchbase/bin/couchbase-cli server-list -c $ip -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD | \
              grep $ip | grep healthy | grep active)
-        echo "debug: server_info: $ip - $output"
+        echo "OUTPUT: server-list: $ip - $output"
         echo ""
 
         if [ -n "$output" ]; then
-          echo "Active server found in the cluster."
+          echo "INFO: Active server found in the cluster."
           PRIMARY_SERVER_IP=$ip
-          echo "Primary server is-1: $PRIMARY_SERVER_IP"
+          echo "INFO: Primary server is-1: $PRIMARY_SERVER_IP"
           break
         fi
 
@@ -334,14 +334,14 @@ get_primary_server () {
     if [ -z "$output" ]; then
         id=`echo $ALL_INST | head -n1 | awk '{print $1;}'`
         PRIMARY_SERVER_IP=$(get_node_ip $arg1 $id)
-        echo "Primary server is-2: $PRIMARY_SERVER_IP"
+        echo "INFO: Primary server is-2: $PRIMARY_SERVER_IP"
     fi
     
 }
 ## ***************************************************************************************************
 
 ## ***************************************************************************************************
-# Still in testing...
+# This will handle one failed node only. 
 find_unhealthy_nodes_and_remove () {
 
     dead_node=$(/opt/couchbase/bin/couchbase-cli server-list -c $PRIMARY_SERVER_IP -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD | \
@@ -352,7 +352,7 @@ find_unhealthy_nodes_and_remove () {
         echo "INFO: Preparing to failover..."
         output=$(/opt/couchbase/bin/couchbase-cli failover -c $PRIMARY_SERVER_IP -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD \
                --server-failover $dead_node --force)
-        echo "INFO: Server failover: $output"
+        echo "OUTPUT: Server failover: $output"
         sleep 10s
     fi
 
@@ -360,39 +360,39 @@ find_unhealthy_nodes_and_remove () {
 ## ***************************************************************************************************
 
 ## ************************** EXECUTION *************************************************************
-echo "01. Create data and index paths..."
+echo "STEP 01. Create data and index paths..."
 create_paths
 echo ""
-echo "02. Get tags..."
+echo "STEP 02. Get tags..."
 get_tags_and_instances
 echo ""
-echo "03. Wait for all the EC2 instances..."
+echo "STEP 03. Wait for all the EC2 instances..."
 wait_for_ec2
 echo ""
-echo "04. Wait for network..."
+echo "STEP 04. Wait for network..."
 wait_for_network
 echo ""
-echo "05. Wait for couchbase servers..."
+echo "STEP 05. Wait for couchbase servers..."
 wait_for_couchbase
 echo ""
-echo "06. Initializes the node, $CURRENT_NODE_IP"
+echo "STEP 06. Initializes the node, $CURRENT_NODE_IP"
 node_init
 echo ""
-echo "07. Identify a primary server..."
+echo "STEP 07. Identify a primary server..."
 get_primary_server
 echo ""
-echo "08. Initializing the cluster..."
+echo "STEP 08. Initializing the cluster..."
 cluster_init
 sleep 10s
 echo ""
-echo "09. Add server..."
+echo "STEP 09. Add server..."
 server_add
 sleep 30s
 echo ""
-echo "10. Remove unhealthy nodes..."
+echo "STEP 10. Remove unhealthy nodes..."
 find_unhealthy_nodes_and_remove
 echo ""
-echo "11. Rebalance"
+echo "STEP 11. Rebalance"
 rebalance
 ## ***********************END OF EXECUTION **********************************************************
 
