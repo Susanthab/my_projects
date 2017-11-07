e77bfc587371a352b22a385660abe072f9270a25
Bathige, Susantha <bathige@hotmail.com>
2017-11-07 11:00:27 -0700

    couchb ud

--- aws/user-data/couchbase_userdata.sh
+++ aws/user-data/couchbase_userdata.sh
@@ -307,8 +307,8 @@ server_status () {
     for id in $ALL_ASG_INST
     do 
         ip=$(get_node_ip $arg1 $id)
-        output=$(/opt/couchbase/bin/couchbase-cli server-info -c $ip -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD | \ 
-            grep clusterMembership | grep active | awk '{print $2}' | tr -d '",')
+        output=$(/opt/couchbase/bin/couchbase-cli server-info -c $ip -u $CLUSTER_USER_NAME -p $CLUSTER_PASSWORD | \
+                 grep clusterMembership | grep active | awk '{print $2}' | tr -d '",')
         echo "debug: server_info: $ip - $output"
     done
 
