046be75a067af4db41bd1dc0d5cbe822398937de
Bathige, Susantha <bathige@hotmail.com>
2017-12-14 21:53:27 -0700

    couch backup

--- couchbase/bitesize-cbbackup.sh
+++ couchbase/bitesize-cbbackup.sh
@@ -110,7 +110,7 @@ get_latest_backup_dir () {
 
 compress (){
   echo "Zipping the backup file..."
-  BACKUP_ZIP=${LATEST_BACKUP_DIR}"$(date -u +"%M-%S")".zip
+  BACKUP_ZIP=${LATEST_BACKUP_DIR}-"$(date -u +"%M-%S")".zip
   #zip -rq -D ${LATEST_BACKUP_DIR}.zip $SOURCE_DIR 
   zip -rq -D ${BACKUP_ZIP} $SOURCE_DIR 
   #BACKUP_ZIP="${LATEST_BACKUP_DIR}.zip"  
